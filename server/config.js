03a4b2d45668fa04528b9ba91206d9cc56782458
trrscld2 <trrscld2@illinois.edu>
2018-05-21 17:15:18 -0500

    Initial commit

---
+++ server/config.js
@@ -0,0 +1,16 @@
+'use strict';
+
+// Autodesk Forge configuration
+module.exports = {
+  // set environment variables or hard-code here
+  credentials: {
+    client_id: process.env.FORGE_CLIENT_ID,
+    client_secret: process.env.FORGE_CLIENT_SECRET,
+    callback_url: process.env.FORGE_CALLBACK_URL
+  },
+
+  // Required scopes for your application on server-side
+  scopeInternal: ['bucket:create', 'bucket:read', 'data:read', 'data:create', 'data:write'],
+  // Required scope of the token sent to the client
+  scopePublic: ['viewables:read']
+};
